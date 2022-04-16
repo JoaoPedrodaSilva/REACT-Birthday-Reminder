695fb7bfcccb4f710ce5bd13e7ffd761f78bf62a
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-04-16 16:42:08 -0300

    better style

--- src/Person.js
+++ src/Person.js
@@ -4,8 +4,10 @@ const Person = ({person}) => {
     const {id, name, age, image} = person
     return (
         <React.Fragment>
-            <div className='person' key={id}>            
-                <img className='image' src={image} alt={name} />            
+            <div className='person' key={id}>
+                <div className='img-container'>
+                    <img className='image' src={image} alt={name} />            
+                </div>         
                 <div>
                     <h4 className='name'>{name}</h4>
                     <h5 className='age'>{age} years old</h5>
--- src/data.js
+++ src/data.js
@@ -1,32 +1,37 @@
 export default [
-    {
-        id: 1,
-        name: 'William Williams',
-        age: 29,
-        image: 'https://img.freepik.com/free-photo/portrait-smart-professional-african-american-man-standing-with-hands-crossed-chest-confident-pose_176420-33861.jpg?size=626&ext=jpg&ga=GA1.2.2083167776.1643562355'
-    },
-    {
-        id: 2,
-        name: 'Roseline Thompson',
-        age: 25,
-        image: 'https://img.freepik.com/free-photo/portrait-beautiful-young-woman-standing-grey-wall_231208-10760.jpg?size=626&ext=jpg&ga=GA1.2.2083167776.1643562355'
-    },
-    {
-        id: 3,
-        name: 'Karen Armstrong',
-        age: 26,
-        image: 'https://img.freepik.com/free-photo/happiness-wellbeing-confidence-concept-cheerful-attractive-african-american-woman-curly-haircut-cross-arms-chest-self-assured-powerful-pose-smiling-determined-wear-yellow-sweater_176420-35063.jpg?size=626&ext=jpg&ga=GA1.2.2083167776.1643562355'
-    },
-    {
-        id: 4,
-        name: 'Donald Johnson',
-        age: 32,
-        image: 'https://img.freepik.com/free-photo/indoor-shot-positive-bearded-male-casual-red-t-shirt-points-with-index-finger-aside_273609-16274.jpg?size=626&ext=jpg&ga=GA1.2.2083167776.1643562355'
-    },
-    {
-        id: 5,
-        name: 'Jeremy Junior',
-        age: 39,
-        image: 'https://img.freepik.com/free-photo/handsome-confident-smiling-man-with-hands-crossed-chest_176420-18743.jpg?size=626&ext=jpg&ga=GA1.1.2083167776.1643562355'
-    },
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
 ]
